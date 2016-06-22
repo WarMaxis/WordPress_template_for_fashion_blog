d510ab178d1d4b694f8bf354fc1723e8765284c1
Michał Milanowski <michalmilanowski@op.pl>
2016-06-22 17:41:41 +0200

    Usunięcie standardowych komentarzy WordPressa

--- dressmakerspin/single.php
+++ dressmakerspin/single.php
@@ -36,36 +36,8 @@
 					
 				</article>	<!-- /.blog-post -->
 				
-				<?php
-				if (is_singular()) {
-				// support for pages split by nextpage quicktag
-				wp_link_pages();
-
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-
-				// tags anyone?
-				the_tags();
-				}
-				?>
 		   		<?php endwhile; ?>
 				
-				<?php if (!is_singular()) : ?>
-				<div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-				<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
-				<?php endif; ?>
-
 				<?php else : ?>
 
 				<article class="subpages-article">
